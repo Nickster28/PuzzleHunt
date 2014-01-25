d18cc273ded668a6ab4bedf44276914356e8f3f2
Nick Troccoli <nicktroccoli28@gmail.com>
2014-01-25 11:35:03 -0800

    Added puzzle creation screen

---
+++ PuzzleHunt/PuzzleHunt/PHCreatedGamesViewController.m
@@ -0,0 +1,75 @@
+//
+//  PHCreatedGamesViewController.m
+//  PuzzleHunt
+//
+//  Created by Nick Troccoli on 1/25/14.
+//  Copyright (c) 2014 Trancos. All rights reserved.
+//
+
+#import "PHCreatedGamesViewController.h"
+
+@interface PHCreatedGamesViewController ()
+@property (nonatomic, strong) NSMutableArray *createdGames;
+@end
+
+@implementation PHCreatedGamesViewController
+@synthesize createdGames = _createdGames;
+
+
+- (NSMutableArray *)createdGames
+{
+    if (!_createdGames) {
+        _createdGames = [[NSMutableArray alloc] init];
+    }
+    
+    return _createdGames;
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    self.navigationController.navigationBarHidden = NO;
+}
+
+
+
+#pragma mark - Table view data source
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.createdGames.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end
