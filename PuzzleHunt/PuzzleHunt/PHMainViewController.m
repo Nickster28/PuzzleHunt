f2e4e1bc71c87ec187362485f05b81fd7194d3e1
Nick Troccoli <nicktroccoli28@gmail.com>
2014-01-25 09:45:40 -0800

    cleaned up code

--- PuzzleHunt/PuzzleHunt/PHMainViewController.m
+++ PuzzleHunt/PuzzleHunt/PHMainViewController.m
@@ -13,15 +13,6 @@
 
 @implementation PHMainViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 
 - (void)viewWillAppear:(BOOL)animated
 {
