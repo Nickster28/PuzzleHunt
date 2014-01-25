c097aa11dcedd75a730033e7ac9caa808aef48be
Nick Troccoli <nicktroccoli28@gmail.com>
2014-01-25 12:39:34 -0800

    Implemented bind cell method for game cell

--- PuzzleHunt/PuzzleHunt/PHGameCell.m
+++ PuzzleHunt/PuzzleHunt/PHGameCell.m
@@ -7,12 +7,18 @@
 //
 
 #import "PHGameCell.h"
+#import "PHGame.h"
 
 @implementation PHGameCell
 
 - (void)bindGame:(PHGame *)game
 {
+    [[self titleLabel] setText:[game gameName]];
     
+    NSUInteger numClues = [[game gameClues] count];
+    [[self numCluesLabel] setText:[NSString stringWithFormat:@"%i Clues", numClues]];
+    [[self durationLabel] setText:[NSString stringWithFormat:@"Time: %i min", [game totalTime]]];
+    [[self descriptionLabel] setText:[game gameDescription]];
 }
 
 @end
