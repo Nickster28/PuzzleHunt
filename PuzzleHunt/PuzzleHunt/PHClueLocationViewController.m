db583075e3ca56201f8112963c493186bf2b5aa9
Nick Troccoli <nicktroccoli28@gmail.com>
2014-01-26 00:48:14 -0800

    Added clue location VC

---
+++ PuzzleHunt/PuzzleHunt/PHClueLocationViewController.m
@@ -0,0 +1,38 @@
+//
+//  PHClueLocationViewController.m
+//  PuzzleHunt
+//
+//  Created by Nick Troccoli on 1/26/14.
+//  Copyright (c) 2014 Trancos. All rights reserved.
+//
+
+#import "PHClueLocationViewController.h"
+
+@interface PHClueLocationViewController ()
+
+@end
+
+@implementation PHClueLocationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
